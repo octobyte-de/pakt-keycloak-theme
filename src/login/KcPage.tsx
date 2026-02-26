fb2fc1982e6c5cd117e4660da0f8fd5998aa5462
Felix <62290842+ctrl-Felix@users.noreply.github.com>
2026-02-26 11:10:51 +0100

    v1 of keycloak mypakt theme

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -3,7 +3,10 @@ import type { ClassKey } from "keycloakify/login";
 import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
-import Template from "keycloakify/login/Template";
+import Template from "./Template";
+
+import "./main.css";
+
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
@@ -37,4 +40,9 @@ export default function KcPage(props: { kcContext: KcContext }) {
     );
 }
 
-const classes = {} satisfies { [key in ClassKey]?: string };
+const classes = {
+    kcButtonClass: "",
+    kcButtonPrimaryClass: "",
+    kcButtonBlockClass: "",
+    kcButtonLargeClass: ""
+} satisfies { [key in ClassKey]?: string };
